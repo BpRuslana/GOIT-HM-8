b842d283cfe1af1eb5981ec8ab57d078fed19a06
BpRuslana <139804632+BpRuslana@users.noreply.github.com>
2023-11-15 19:06:00 +0100

    all

---
+++ src/js/02-video.js
@@ -0,0 +1,29 @@
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function setTime(e){
+    const seconds = e.seconds;
+    localStorage.setItem('videoplayer-current-time', seconds)
+}
+const throttledFunc = throttle(setTime, 1000);
+player.on('timeupdate',throttledFunc);
+
+function onLoadWindow(){
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+
+    player.setCurrentTime(savedTime).then(function(seconds) {
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+            default:
+                break;
+        }
+    });
+}
+onLoadWindow();
